42a5180ba99f983ef8ea164ce564bf93a52bf3e9
Diogo Gomes <dgomes@graphnode.com>
2015-12-15 01:02:27 +0000

    Added code for merging into mbox after downloading messages. Refactored code.

--- src/getAuth2Client.js
+++ src/getAuth2Client.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    open = require('open'),
     readline = require('readline');
 
 var GoogleAuth = require('google-auth-library');
@@ -32,7 +33,10 @@ function getNewToken(oauth2Client, callback) {
         access_type: 'offline',
         scope: SCOPES
     });
-    console.log('Authorize this app by visiting this url: ', authUrl);
+    
+    console.log('Opening browser to get authorization...');
+    open(authUrl);
+    
     var rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -40,10 +44,9 @@ function getNewToken(oauth2Client, callback) {
     rl.question('Enter the code from that page here: ', function (code) {
         rl.close();
         oauth2Client.getToken(code, function (err, token) {
-            if (err) {
-                console.log('Error while trying to retrieve access token', err);
-                return;
-            }
+            if (err)
+                throw 'Error while trying to retrieve access token ' + err;
+                
             oauth2Client.credentials = token;
             storeToken(token);
             callback(undefined, oauth2Client);
@@ -66,18 +69,14 @@ function storeToken(token) {
 module.exports = function (callback) {
     // Load client secrets from a local file.
     fs.readFile(SECRET_PATH, 'utf8', function (err, content) {
-        if (err) {
-            callback('Error loading client secret file: ' + err, null);
-            return;
-        }
+        if (err)
+            throw 'Error loading client secret file: ' + err;
 
         // Authorize a client with the loaded credentials, then call the
         // Gmail API.
         authorize(JSON.parse(content), function(err, authClient) {
-            if (err) {
-                callback('Error getting auth client: ' + err, null);
-                return;
-            }
+            if (err)
+                throw 'Error getting auth client: ' + err;
 
             callback(null, authClient);
         });
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-var shell = require('shelljs'),
+var fs = require('fs'),
+    shell = require('shelljs'),
     base64url = require('base64url'),
     moment = require('moment'),
-    async = require('async');
+    async = require('async'),
+    ProgressBar = require('progress');
 
 var google = require('googleapis'),
     gmail = google.gmail('v1'),
     getAuth2Client = require('./getAuth2Client');
 
-if (!shell.test('-e', 'mbox')) {
-    shell.mkdir('mbox');
-}
+// TODO: This should be from the command line:
+var maxPageResults = 100; // How many messages per page.
+var maxMessages = 0; // 0 for all.
 
 getAuth2Client(function(err, auth) {
     console.log('Getting profile and labels...');
@@ -23,6 +25,10 @@ getAuth2Client(function(err, auth) {
         
         var messagesTotal = profile.messagesTotal;
         var messagesCounter = 0;
+        
+        if (!shell.test('-e', 'mbox_' + profile.emailAddress)) {
+            shell.mkdir('mbox_' + profile.emailAddress);
+        }
         
         var normalizeLabel = function(str) {
             if (str !== str.toUpperCase()) return str;
@@ -39,38 +45,39 @@ getAuth2Client(function(err, auth) {
         };
         
         var writeMessageToFile = function(message) {
-            var wstream = require('fs').createWriteStream('mbox/' + message.id + '.eml');
+            var wstream = fs.createWriteStream('mbox_' + profile.emailAddress + '/' + message.id + '.mbox.part', { flags: 'w' });
             
             var threadId = parseInt(message.threadId, 16);
             var date = moment(parseInt(message.internalDate, 10)).format('ddd MMM DD HH:mm:ss YYYY');
-            var labelStr = getLabelsFromMessage(labels, message);
+            var labelStr = getLabelsFromMessage(message);
             
-            //wstream.write('From ' + threadId + '@xxx ' + date + '\r\n');
+            wstream.write('From ' + threadId + '@xxx ' + date + '\r\n');
             wstream.write('X-GM-THRID: ' + threadId + '\r\n');
-            wstream.write('X-GM-DATE: ' + date + '\r\n');
             
             if (labelStr && labelStr.length != 0) {
                 wstream.write('X-Gmail-Labels: ' + labelStr + '\r\n');
             }
             
             wstream.write(base64url.decode(message.raw));
-            //wstream.write('\r\n');
+            wstream.write('\r\n');
                             
             wstream.end();
         };
-                
-        console.log('Getting messages...');
+        
+        var downloadBar = new ProgressBar('Getting messages... [:bar] :percent - (:current/:total) - :eta seconds left.', { 
+            total: Math.min(maxMessages || messagesTotal, messagesTotal), 
+            complete: '=', 
+            incomplete: ' ',
+            width: 40 
+        });
 
-
-        var maxPageResults = 200;
         var nextPageToken = undefined;
         
         // While there is a nextPageToken:
         async.doWhilst(function(pageCallback) {
-            async.retry({times: 5, interval: 500}, async.apply(gmail.users.messages.list, { auth: auth, userId: 'me', maxResults: maxPageResults, pageToken: nextPageToken }), function(err, response) {
-                if (err) {
-                    throw 'Failed getting message list! ' + nextPageToken;    
-                }
+            async.retry({times: 20, interval: 1000}, async.apply(gmail.users.messages.list, { auth: auth, userId: 'me', maxResults: maxPageResults, pageToken: nextPageToken }), function(err, response) {
+                if (err)
+                    throw 'Failed getting message list: ' + nextPageToken + ' - ' + err;    
                 
                 nextPageToken = response.nextPageToken;
                 var messageIds = response.messages.map(function(m) { return m.id; });
@@ -79,33 +86,67 @@ getAuth2Client(function(err, auth) {
                 async.eachSeries(messageIds, function(messageId, callback) {
                     messagesCounter++;
                     
-                    if (shell.test('-e', 'mbox/' + messageId + '.eml')) {
-                        console.log('Skipped message ' + messagesCounter + ' of ' + messagesTotal + ': File found.');
+                    if (maxMessages && messagesCounter > maxMessages) {
+                        async.setImmediate(function () { callback(); });
+                        return;
+                    }
+
+                    var partFilename = 'mbox_' + profile.emailAddress + '/' + messageId + '.mbox.part';
+
+                    if (shell.test('-f', partFilename) && fs.statSync(partFilename).size != 0) {
+                        downloadBar.tick(1);
                         async.setImmediate(function () { callback(); });
                         return;
                     }
                     
-                    async.retry({times: 5, interval: 500}, async.apply(gmail.users.messages.get, { auth: auth, userId: 'me', id: messageId, format: 'raw' }), function(err, message) {
-                        if (err) {
-                            throw 'Failed getting message! ' + messageId;    
-                        }
+                    async.retry({times: 20, interval: 1000}, async.apply(gmail.users.messages.get, { auth: auth, userId: 'me', id: messageId, format: 'raw' }), function(err, message) {
+                        if (err)
+                            throw 'Failed getting message: ' + message.id + ' - ' + err;    
                 
                         writeMessageToFile(message);
-                        console.log('Got message ' + messagesCounter + ' of ' + messagesTotal + '.');
+                        
+                        downloadBar.tick(1);
                         callback();
                     });
                 }, function(err) {
-                    if (err) {
+                    if (err)
                         throw 'Failed looping messages! ' + messageIds;
-                    }
                     
                     pageCallback();
                 });
             });
-        }, function() { return nextPageToken !== undefined; },
-        function() { 
-            console.log('All done!');
-            process.exit(0);
+        }, 
+        function() { return nextPageToken !== undefined && (!maxMessages || messagesCounter < maxMessages); },
+        function() {
+            var resultFilename = profile.emailAddress + '.mbox';
+            var partFilenames = shell.ls('mbox_' + profile.emailAddress + '/*.mbox.part');
+            
+             var mergeBar = new ProgressBar('Merging files into a mbox... [:bar] :percent - (:current/:total) - :eta seconds left.', { 
+                total: partFilenames.length, 
+                complete: '=', 
+                incomplete: ' ',
+                width: 40 
+            });
+            
+            var wstream = fs.createWriteStream(resultFilename, { flags: 'w', defaultEncoding: 'utf8' });
+            
+            async.eachSeries(partFilenames, function(partFilename, callback) {
+                var rstream = fs.createReadStream(partFilename, { flags: 'r'});
+                rstream.pipe(wstream, { end: false });
+                mergeBar.tick(1);
+                rstream.on('end', callback);
+            }, function(err) {
+                if (err)
+                    throw 'Failed to merge files: ' + err;
+                
+                wstream.end();
+            
+                console.log('Cleaning up...')
+                shell.rm('-rf', 'mbox_' + profile.emailAddress);
+                
+                console.log('All done! File ' + resultFilename + ' was created.');
+                process.exit(0);
+            });
         });
     });
 });
